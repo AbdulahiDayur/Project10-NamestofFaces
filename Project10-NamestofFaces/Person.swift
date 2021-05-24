3d14bac8b7084fe835c9ba19ad96ababf786a275
Abdul Dayur <Abdul.dayur@nike.com>
2021-05-24 13:26:03 -0700

    created func that adds files to hidden directory

---
+++ Project10-NamestofFaces/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  Project10-NamestofFaces
+//
+//  Created by Abdul Dayur on 5/24/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
--- Project10-NamestofFaces/ViewController.swift
+++ Project10-NamestofFaces/ViewController.swift
@@ -5,12 +5,15 @@
 //  Created by Abdul Dayur on 5/22/21.
 //
 
+
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         
     }
     
@@ -27,6 +30,36 @@ class ViewController: UICollectionViewController {
         
         return cell
     }
+    
+    // let users select image from camera to import into an app
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self //respond to messages from picker
+        present(picker, animated: true)
+    }
+    
+    //returns when the user selected an image and its being returned to you.
+    func imagePickerController(_ picker: UIImagePickerController, 
+       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return} //extract image from dictionary
+        
+        //Generating a Unique File Name for our disk
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName) //Appending our filename to hidden directory
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath) //write to disk
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
 
 
 }
