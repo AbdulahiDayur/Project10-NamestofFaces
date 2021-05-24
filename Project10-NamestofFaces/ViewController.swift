25b9ed3c2a92225af8f61b5d62ed131330985596
Abdul Dayur <Abdul.dayur@nike.com>
2021-05-23 17:17:06 -0700

    adding basic implementation for collection	 view cell

--- Project10-NamestofFaces/ViewController.swift
+++ Project10-NamestofFaces/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath)
+        
+        return cell
+    }
 
 
 }
